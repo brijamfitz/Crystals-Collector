624b860d7fadbd7b3f8ef3366c928c3834805d8d
brijamfitz <brijamfitz@Brians-MacBook-Pro.local>
2018-08-23 15:50:25 -0400

    work in progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,29 +1,114 @@
 $(document).ready(function () {
 
 // GLOBAL VARIABLES
-// ===================================================
+// =====================================================================
 var wins = 0;
 var losses = 0;
 var counter = 0;
 var targetNumber;
 
-$("#total-wins").append("Wins: " + wins); // Need to replace, not append
-$("#total-losses").append("Losses: " + losses); // Need to replace, not append
-
-// MAIN
-// ===================================================
+$("#total-wins").html("Wins: " + wins); 
+$("#total-losses").html("Losses: " + losses);
 
 // Reset game function
 function resetGame() {
-    $("#number-to-guess").empty();
-    $("#total-score").empty();
+    
+    // Randomly generate target number between 19-120
+    targetNumber = Math.floor((Math.random() * 120) + 19);
+    console.log("This is the target number: " + targetNumber);
+
+    // Push target number to dom using jquery
+    $("#number-to-guess").html(targetNumber);
+
+    // Randomly generate a number from 1-12 and assign to variable
+    var numberOption1 = Math.floor((Math.random() * 12) + 1);
+    console.log("This is the crystal 1 value: " + numberOption1);
+
+    var numberOption2 = Math.floor((Math.random() * 12) + 1);
+    console.log("This is the crystal 2 value: " + numberOption2);
+    
+    var numberOption3 = Math.floor((Math.random() * 12) + 1);
+    console.log("This is the crystal 3 value: " + numberOption3);
+    
+    var numberOption4 = Math.floor((Math.random() * 12) + 1);
+    console.log("This is the crystal 4 value: " + numberOption4);
+
+    $(".crystal-image-1").attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
+
+    $(".crystal-image-2").attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
+
+    $(".crystal-image-3").attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
+
+    $(".crystal-image-4").attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
+
+    $(".crystal-image-1").attr("data-crystalvalue", numberOption1);
+    $(".crystal-image-2").attr("data-crystalvalue", numberOption2);
+    $(".crystal-image-3").attr("data-crystalvalue", numberOption3);
+    $(".crystal-image-4").attr("data-crystalvalue", numberOption4);
+    
+    $(".crystal-image-1").on("click", function() {
+
+        var crystalValue = ($(".crystal-image-1").attr("data-crystalvalue"));
+        crystalValue = parseInt(crystalValue);
+
+        if (counter === targetNumber) {
+            resetGame();
+        }
+        else if (counter > targetNumber) {
+            resetGame(); 
+        }
+    })
+    
+    $(".crystal-image-2").on("click", function() {
+
+        var crystalValue = ($(".crystal-image-2").attr("data-crystalvalue"));
+        crystalValue = parseInt(crystalValue);
+
+        if (counter === targetNumber) {
+            resetGame();
+        }
+        else if (counter > targetNumber) {
+            resetGame(); 
+        }
+    })
+
+    $(".crystal-image-3").on("click", function() {
+
+        var crystalValue = ($(".crystal-image-3").attr("data-crystalvalue"));
+        crystalValue = parseInt(crystalValue);
+
+        if (counter === targetNumber) {
+            resetGame();
+        }
+        else if (counter > targetNumber) {
+            resetGame(); 
+        }
+    })
+
+    $(".crystal-image-4").on("click", function() {
+
+        var crystalValue = ($(".crystal-image-4").attr("data-crystalvalue"));
+        crystalValue = parseInt(crystalValue);
+
+        if (counter === targetNumber) {
+            resetGame();
+        }
+        else if (counter > targetNumber) {
+            resetGame(); 
+        }
+    })
+
+    }
+   
+// MAIN
+// =====================================================================
 
     // Randomly generate target number between 19-120
     var targetNumber = Math.floor((Math.random() * 120) + 19);
     console.log("This is the target number: " + targetNumber);
 
     // Push target number to dom using jquery
-    $("#number-to-guess").append(targetNumber);
+    $("#number-to-guess").html(targetNumber);
 
     // Randomly generate a number from 1-12 and assign to variable
     var numberOption1 = Math.floor((Math.random() * 12) + 1);
@@ -38,86 +123,29 @@ function resetGame() {
     var numberOption4 = Math.floor((Math.random() * 12) + 1);
     console.log("This is the crystal 4 value: " + numberOption4);
     
-    // Store those four numbers in array
-    var numberOptions = [numberOption1, numberOption2, numberOption3, numberOption4];
+    // Give each imageCrystal an src link to image of crystal
+    $(".crystal-image-1").attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
 
-    // Loop through array and create crystals for every number option
-    for (var i = 0; i < numberOptions.length; i++) {
-        console.log(numberOptions[i]);
-        
-        // For each iteration, create an imageCrystal
-        // var imageCrystal = $("<img>");
+    $(".crystal-image-2").attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
 
-        // Give each crystal the class .crystal-image
-        // imageCrystal.addClass("crystal-image");
+    $(".crystal-image-3").attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
 
-        // Give each imageCrystal an src link to image of crystal
-        // imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
+    $(".crystal-image-4").attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
 
-        // Give each imageCrystal an attribute called data-cyrstalValue
-        imageCrystal.empty("data-crystalvalue");
-
-        // imageCrystal.append("data-crystalvalue", numberOptions[i]);
-
-        // Add each crystalImage to html id
-        // $("#crystals").append(imageCrystal);
-        
-    }
-
-}
-
-    // Randomly generate target number between 19-120
-    var targetNumber = Math.floor((Math.random() * 120) + 19);
-    console.log("This is the target number: " + targetNumber);
-
-    // Push target number to dom using jquery
-    $("#number-to-guess").append(targetNumber);
-
-    // Randomly generate a number from 1-12 and assign to variable
-    var numberOption1 = Math.floor((Math.random() * 12) + 1);
-    console.log("This is the crystal 1 value: " + numberOption1);
-
-    var numberOption2 = Math.floor((Math.random() * 12) + 1);
-    console.log("This is the crystal 2 value: " + numberOption2);
-    
-    var numberOption3 = Math.floor((Math.random() * 12) + 1);
-    console.log("This is the crystal 3 value: " + numberOption3);
-    
-    var numberOption4 = Math.floor((Math.random() * 12) + 1);
-    console.log("This is the crystal 4 value: " + numberOption4);
-    
-    // Store those four numbers in array
-    var numberOptions = [numberOption1, numberOption2, numberOption3, numberOption4];
-
-    // Loop through array and create crystals for every number option
-    for (var i = 0; i < numberOptions.length; i++) {
-        console.log(numberOptions[i]);
-        
-        // For each iteration, create an imageCrystal
-        var imageCrystal = $("<img>");
-
-        // Give each crystal the class .crystal-image
-        imageCrystal.addClass("crystal-image");
-
-        // Give each imageCrystal an src link to image of crystal
-        imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
-
-        // Give each imageCrystal an attribute called data-cyrstalValue
-        imageCrystal.attr("data-crystalvalue", numberOptions[i]);
-
-        // Add each crystalImage to html id
-        $("#crystals").append(imageCrystal);
-        
-    }
+    // Give each imageCrystal an attribute called data-crystalValue
+    $(".crystal-image-1").attr("data-crystalvalue", numberOption1);
+    $(".crystal-image-2").attr("data-crystalvalue", numberOption2);
+    $(".crystal-image-3").attr("data-crystalvalue", numberOption3);
+    $(".crystal-image-4").attr("data-crystalvalue", numberOption4);
 
     // Click event will apply to all crystals
-    $(".crystal-image").on("click", function() {
+    $(".crystal-image-1").on("click", function() {
 
         // Determining the crystal's value requires us to extract the value from the data attribute.
         // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
         // Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
         // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
-        var crystalValue = ($(this).attr("data-crystalvalue"));
+        var crystalValue = ($(".crystal-image-1").attr("data-crystalvalue"));
         crystalValue = parseInt(crystalValue);
 
         // We then add the crystalValue to the user's "counter" which is a global variable.
@@ -125,21 +153,121 @@ function resetGame() {
         counter += crystalValue;
 
         // When user clicks on a gem, add its value to the total score, and for each subsequent gem click, add that value on to total score
-        // Extract value of each gem
-        $('#total-score').empty().append(counter); // Need to replace, not append
+        $('#total-score').html(counter);
 
         // Win/lose statements
         if (counter === targetNumber) {
+            counter = 0;
             wins++;
-            alert("You win!");  
-            resetGame();   
+            $("#total-wins").html("Wins: " + wins);
+            alert("You win!");
+            $("#number-to-guess").empty();
+            $("#total-score").empty();
+            resetGame();         
         }
         else if (counter > targetNumber) {
+            counter = 0;
             losses++;
+            $("#total-losses").html("Losses: " + losses);
             alert("You lose!");
+            $("#number-to-guess").empty();
+            $("#total-score").empty();
             resetGame();
         }
 
     })
+
+    $(".crystal-image-2").on("click", function() {
+
+        var crystalValue = ($(".crystal-image-2").attr("data-crystalvalue"));
+        crystalValue = parseInt(crystalValue);
+
+        counter += crystalValue;
+
+        $('#total-score').html(counter);
+
+        // Win/lose statements
+        if (counter === targetNumber) {
+            counter = 0;
+            wins++;
+            $("#total-wins").html("Wins: " + wins);
+            alert("You win!");
+            $("#number-to-guess").empty();
+            $("#total-score").empty();
+            resetGame();
+        }
+        else if (counter > targetNumber) {
+            counter = 0;
+            losses++;
+            $("#total-losses").html("Losses: " + losses);
+            alert("You lose!");
+            $("#number-to-guess").empty();
+            $("#total-score").empty();
+            resetGame();
+        }
+
+    })
+
+    $(".crystal-image-3").on("click", function() {
+
+        var crystalValue = ($(".crystal-image-3").attr("data-crystalvalue"));
+        crystalValue = parseInt(crystalValue);
+
+        counter += crystalValue;
+
+        $('#total-score').html(counter);
+
+        // Win/lose statements
+        if (counter === targetNumber) {
+            counter = 0;
+            wins++;
+            $("#total-wins").html("Wins: " + wins);
+            alert("You win!");
+            $("#number-to-guess").empty();
+            $("#total-score").empty();          
+            resetGame();
+        }
+        else if (counter > targetNumber) {
+            counter = 0;
+            losses++;
+            $("#total-losses").html("Losses: " + losses);
+            alert("You lose!");
+            $("#number-to-guess").empty();
+            $("#total-score").empty();
+            resetGame();
+        }
+
+    })
+
+    $(".crystal-image-4").on("click", function() {
+
+        var crystalValue = ($(".crystal-image-4").attr("data-crystalvalue"));
+        crystalValue = parseInt(crystalValue);
+
+        counter += crystalValue;
+
+        $('#total-score').html(counter);
+
+        // Win/lose statements
+        if (counter === targetNumber) {
+            counter = 0;
+            wins++;
+            $("#total-wins").html("Wins: " + wins);
+            alert("You win!");
+            $("#number-to-guess").empty();
+            $("#total-score").empty();          
+            resetGame();
+        }
+        else if (counter > targetNumber) {
+            counter = 0;
+            losses++;
+            $("#total-losses").html("Losses: " + losses);
+            alert("You lose!");
+            $("#number-to-guess").empty();
+            $("#total-score").empty();            
+            resetGame();
+        }
+
+    })  
 
     });
